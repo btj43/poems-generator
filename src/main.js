50364d20e1c25f388035077bfe0663f3ee6b4c63
btj43 <bethjones543@gmail.com>
2025-09-24 09:50:44 +1000

    add basic javascript

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+let poemForm = document.querySelector("#poem-form");
+poemForm.addEventListener("submit", generatePoem);
 
+function generatePoem(event) {
+  console.log("in generate poem");
+  event.preventDefault();
+  let poemElement = document.querySelector("#poem");
+
+  new Typewriter("#poem", {
+    strings: "hello world",
+    autoStart: true,
+  });
+}
